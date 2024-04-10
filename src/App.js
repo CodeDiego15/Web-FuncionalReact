aedff414ef8d893db34851d16a534b2c3e52644b
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-04-10 00:46:28 -0400

    Commit To Main

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import LoginButton from './Login';
+import logo from './logo.svg';
+import './App.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
